acd21184eab884108a70f3004b0813c5ca206c0b
Aneel Kumar <aneelkkhatri@users.noreply.github.com>
2017-02-05 16:02:56 +0500

    Update time-track-01.js

--- scripts/time-track-01.js
+++ scripts/time-track-01.js
@@ -1,41 +1,12 @@
-Extension.insertCSS({
-    code: `#my-timer-container {
-        position: fixed;
-        left: 0px;
-        top: 0px;
-        padding: 6px;
-        z-index: 999999;
-        color: #00ff00;
-    }`
-});
 
-var timerContainer = document.createElement("div");
-timerContainer.id="my-timer-container";
-document.body.appendChild(timerContainer);
-
-var MS_SEC = 1000;
-var MS_MIN = MS_SEC * 60;
-var MS_HR = MS_MIN * 60;
-
+var originalTitle = document.title;
 function renderTimeSpent() {
-    var s = " ";
-    var t = timeSpent;
-    var unit = "secs";
-    
-    if (t > MS_HR) {
-        t = t / MS_HR;
-        unit = "hrs";
-    }
-    else if (t > MS_MIN) {
-        t = t / MS_MIN;
-        unit = "mins";
-    }
-    else {
-        t = t / MS_SEC
-        unit = "secs";
-    }
-    s += t + " " + unit;
-    timerContainer.textContent = s;
+    t = timeSpent;
+    var sec = (t / 1000);
+    var min = sec / 60;
+    var hr = min / 60;
+    s = parseInt(hr % 24) + ":" + parseInt(min % 60) + ":" + parseInt(sec % 60);
+    document.title = s + " | " + originalTitle;
 }
 var timeSpent = 0;
 var interval = 1000;
@@ -43,7 +14,6 @@ var lastKeyActivityTime = Date.now();
 var lastMouseActivityTime = Date.now();
 function onKeyActivity() { 
 	lastKeyActivityTime = Date.now();
-	console.log('k');
 }
 function onMouseActivity() {
 	lastMouseActivityTime = Date.now();
@@ -54,7 +24,7 @@ document.addEventListener('mousedown', onMouseActivity);
 document.addEventListener('mousewheel', onMouseActivity);
 setInterval(function () {
     var now = Date.now();
-    if (!document.hidden && (document.hasFocus() || now - lastKeyActivityTime < 5000 || now - lastMouseActivityTime < 5000)) {
+    if (!document.hidden && (document.hasFocus() || now - lastKeyActivityTime < 1000 || now - lastMouseActivityTime < 1000)) {
         timeSpent += interval;
     }
     renderTimeSpent();
